7a055829ae403ebe7c794de37faf28219cef7512
Uttam Sharma <mail@uttamsharma.com>
2025-10-05 00:00:41 -0700

    working user sign up and login interaction with backend

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,15 +4,19 @@ import '../styles/homepage.css'
 function HomePage() {
   const navigate = useNavigate()
 
+  const handleButtonClick = () => {
+    navigate('/login')
+  }
+
   return (
     <div className="v1_4">
       <div className="text-content">
         <span className="v9_2">
           Hippos organize your fridge, so recipes are just one chomp away!
         </span>
-        <div className="v14_2">
+        <div className="v14_2" onClick={handleButtonClick}>
           <div className="v6_195"></div>
-          <span className="v6_199">Swipe to your hippo fridge</span>
+          <span className="v6_199">Click for your hippo fridge</span>
         </div>
       </div>
       
